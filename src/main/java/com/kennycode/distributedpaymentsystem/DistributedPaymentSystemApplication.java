19ee1e11173deb06011fc3c2769323373ccbe2c4
Kenneth.Edoho <tibeleedoho007@yahoo.com>
2026-06-09 16:16:32 +0100

    Project setup

---
+++ src/main/java/com/kennycode/distributedpaymentsystem/DistributedPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kennycode.distributedpaymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedPaymentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedPaymentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kennycode/distributedpaymentsystem/DistributedPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennycode.distributedpaymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedPaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
